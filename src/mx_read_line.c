aefc2a773a82c5a65a2873509fbfd6f242b6cf97
hyperconscious <kklinaev@gmail.com>
2023-11-08 02:47:06 +0300

    rwk few functions

--- src/mx_read_line.c
+++ src/mx_read_line.c
@@ -1,57 +1,70 @@
 #include "../inc/libmx.h"
 
 int mx_read_line(char **lineptr, size_t buf_size, char delim, const int fd){
-    if (fd < 0 || !lineptr) {
+    if (!lineptr || buf_size < 1 || buf_size > 2147483647 || fd < 0) {
         return -2; 
     }
 
-    ssize_t read_bytes = 0;
-    size_t line_len = 0;
+    static char* str_rest = NULL;
+    char *text = NULL;
+    size_t text_len = 0;
+
+    if(str_rest != NULL){
+        text = str_rest;
+        int delimIndex = mx_get_char_index(text, delim);
+        if(delimIndex > -1){
+            text[delimIndex] = '\0';
+            str_rest = mx_strdup(&text[delimIndex] + 1);
+        }
+        else {
+            str_rest = NULL;
+        }
+
+        char* text_ptr = text;
+        text_len = mx_strlen(text);
+        text = mx_strndup(text, text_len);
+        mx_strdel(&text_ptr);
+        if(str_rest != NULL){
+            if(mx_strlen(str_rest) == 0)
+                mx_strdel(&str_rest);
+            *lineptr = text;
+            return text_len;
+        }
+
+    }
+
     char *line = mx_strnew(buf_size);
-    char c;
+    ssize_t read_bytes = -1;
 
-    while (1) {
-        if (read(fd, &c, 1) != 1) {
-            if (read_bytes == 0) {
-                *lineptr = line;
-                return -1; 
-            }
+    while((read_bytes = read(fd, line, buf_size)) > 0){
+        int delimIndex = mx_get_char_index(line, delim);
+        if(delimIndex > -1){
+            line[delimIndex] = '\0';
+            str_rest = mx_strdup(&line[delimIndex] + 1);
+        }
+        else {
+            str_rest = NULL;
+        }
+
+        char* text_ptr = text;
+        text_len += mx_strlen(line);
+        text = mx_strjoin(text, line);
+        mx_strdel(&text_ptr);
+        if(str_rest != NULL){
+            if(mx_strlen(str_rest) == 0)
+                mx_strdel(&str_rest);
             break;
         }
-
-        if (c == delim) {
-            break;
-        }
-
-        if (line_len + 1 >= buf_size) {
-            buf_size *= 2;
-            char *temp = mx_realloc(line, buf_size);
-            if (!temp) {
-                free(line);
-                line = NULL;
-                return -2; 
-            }
-            line = temp;
-        }
-
-        line[line_len++] = c;
-        read_bytes++;
     }
 
-    if (line_len == 0) {
-        if (!line) {
-            line = mx_strnew(1); 
-            if (!line) {
-                return -2; 
-            }
-        }
-        line[0] = '\0';
-    } else {
-        line[line_len] = '\0';
+    if(read_bytes == -1){
+        mx_strdel(&text);
+        mx_strdel(&str_rest);
+        return -2;
     }
+    if(text == NULL) return -1;
 
-    mx_strncpy(*lineptr, line, line_len );
-    free(line);
-    return read_bytes;
+    *lineptr = text;
+    return text_len;
 }
 
--- src/mx_strjoin.c
+++ src/mx_strjoin.c
@@ -6,7 +6,7 @@ char *mx_strjoin(const char *s1, const char *s2){
     if(s1 == NULL && s2 == NULL) return NULL;
 
     char *res = mx_strnew(mx_strlen(s1) + mx_strlen(s2));
-    res = mx_strcat(res, s1);
+    res = mx_strcpy(res, s1);
     res = mx_strcat (res, s2);
     return res;
 }
